d2fd7794c2e8dddd4a6725958eb388c31d6c7a7f
verakristina <verakristina784@gmail.com>
2022-01-12 09:32:45 +0700

    Revert "database new config"
    This reverts commit b311e27a351655db5df07eda291c8e2ec57e1844.

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '10.10.10.40',
+	'username' => 'local',
+	'password' => 'Indonesia123',
 	'database' => '1110_kontribusi_ol',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
